0237dd4a6d239dae5b3271cbc395f22139a85a62
ByronTechnology <ByronTechnology@gmail.com>
2021-09-19 17:26:35 -0400

    Commiting

---
+++ back-end/src/table/tables.service.js
@@ -0,0 +1,42 @@
+const knex = require('../db/connection');
+const reservationsService = require('../reservations/reservations.service');
+
+const tableName = 'tables';
+
+function create(newTable) {
+  return knex(tableName)
+    .insert(newTable)
+    .returning('*')
+    .then((createdTable) => createdTable[0]);
+}
+
+function list() {
+  return knex(tableName).select('*').orderBy('table_name', 'asc');
+}
+
+function read(table_id) {
+  return knex(tableName).where({ table_id }).first();
+}
+
+function update(updatedTable) {
+  return knex(tableName)
+    .where({ table_id: updatedTable.table_id })
+    .update(updatedTable, '*')
+    .returning('*')
+    .then((theNewStuff) => theNewStuff[0]);
+}
+
+function destroy(table_id, reservationId) {
+  return knex(tableName)
+    .where({ table_id })
+    .update('reservation_id', null)
+    .then(() => reservationsService.update(reservationId, 'finished'));
+}
+
+module.exports = {
+  create,
+  list,
+  read,
+  update,
+  destroy,
+};
--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -4,14 +4,11 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import Dashboard from "../dashboard/Dashboard";
 import NotFound from "./NotFound";
 import { today } from "../utils/date-time";
+import ReservationForm from "../reservations/ReservationForm";
+import TablesForm from "../tables/TableForm";
+import Seating from "../dashboard/Seating";
+import Search from "../dashboard/Search";
 
-/**
- * Defines all the routes for the application.
- *
- * You will need to make changes to this file.
- *
- * @returns {JSX.Element}
- */
 function Routes() {
   return (
     <Switch>
@@ -21,8 +18,23 @@ function Routes() {
       <Route exact={true} path="/reservations">
         <Redirect to={"/dashboard"} />
       </Route>
+      <Route exact={true} path="/reservations/new">
+        <ReservationForm type="Create a new" />
+      </Route>
       <Route path="/dashboard">
         <Dashboard date={today()} />
+      </Route>
+      <Route path="/tables/new">
+        <TablesForm />
+      </Route>
+      <Route path="/reservations/:reservation_id/seat">
+        <Seating />
+      </Route>
+      <Route path="/reservations/:reservation_id/edit">
+        <ReservationForm type="Edit" />
+      </Route>
+      <Route path="/search">
+        <Search />
       </Route>
       <Route>
         <NotFound />
